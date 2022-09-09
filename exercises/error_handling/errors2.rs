a7d2b5d74d982996dbda17e4322d38adcece78ed
ahokp <77104952+ahokp@users.noreply.github.com>
2022-09-09 11:32:06 +0300

    Done until quiz 3

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -17,7 +17,6 @@
 // one is a lot shorter!
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
@@ -26,7 +25,11 @@ pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>();
 
-    Ok(qty * cost_per_item + processing_fee)
+    let check_int = match qty{
+        Ok(i) => Ok(i*cost_per_item + processing_fee),
+        Err(i) => Err(i),
+    };
+    return check_int;
 }
 
 #[cfg(test)]
